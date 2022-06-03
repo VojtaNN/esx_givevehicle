48fa8b60d42f0ba15c88c10c60cd29e1a363a083
VojtaNN <61760213+VojtaNN@users.noreply.github.com>
2022-06-03 22:35:39 +0200

    Add files via upload

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+Locales['en'] = {
+	['gived_car'] = 'Vehicle ~y~%s ~s~with plate number  %s ~s~has been park into ~g~%s~s~\'s garage',
+	['received_car'] = 'Youi received a vehcle with plate number ~y~%s',
+	['del_car'] = 'Has deleted a vehicle with plate number %s',	
+	['del_car_error'] = 'Can\'t find vehicle with plate is %s',	
+	['unknown_car'] = '~r~Unknown vehicle model %s',
+	['plate_already_have'] = '~r~This plate is already been used on another vehicle'
+}
